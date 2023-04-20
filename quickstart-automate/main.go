06ef5097f93753ce5eebfdc37c20a58ffb9d0d5f
AASHISH PATIL <aashishpatil@google.com>
2023-04-20 19:12:22 +0530

    update text

--- quickstart-automate/main.go
+++ quickstart-automate/main.go
@@ -19,5 +19,5 @@ import (
 )
 
 func main () {
-  fmt.Println("Hello, This is my new test for eventarc trigger project!")
+  fmt.Println("Hello, This is my new test for eventarc trigger project-new!")
 }
