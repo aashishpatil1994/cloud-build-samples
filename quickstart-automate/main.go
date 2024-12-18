6f6e53ff95048493cfced3efffae74f21e3cfc90
aashishpatil1994 <96226341+aashishpatil1994@users.noreply.github.com>
2024-12-18 12:49:52 +0530

    Update main.go

--- quickstart-automate/main.go
+++ quickstart-automate/main.go
@@ -19,5 +19,5 @@ import (
 )
 
 func main () {
-  fmt.Println("Hello,this is test trigger for us-central1 trigger!")
+  fmt.Println("Hello,this is test trigger for us-central1 trigger for adnan!")
 }
